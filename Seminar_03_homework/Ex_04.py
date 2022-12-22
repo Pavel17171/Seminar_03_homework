04c866f42ff06d73c2dca60acb5425fd881232ad
[1] <[1.asus.rfrf]>
2022-12-22 22:03:20 +0300

    Add folder Seminar_03_homework

---
+++ Seminar_03_homework/Ex_04.py
@@ -0,0 +1,13 @@
+﻿# Ex_4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#     Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+number = int(input("Введите число: "))
+number2 = str(number % 2)
+while (number >= 2):
+    number //= 2
+    number2 += str(number % 2)
+number2 = int(number2[::-1])
+print(number2)
---
+++ Seminar_03_homework/Ex_05.py
@@ -0,0 +1,17 @@
+﻿# Ex_5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+#     Пример:
+# - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+# [Негафибоначчи]
+
+number = int(input("Введите число: "))
+list_fib = []
+list_negfib = []
+for i in range(number + 1):
+    if i < 2:
+        list_fib.append(i)
+        list_negfib.append(-i)
+    else:
+        list_fib.append(list_fib[i - 1] + list_fib[i - 2])
+        list_negfib.append(-list_fib[i])        
+list_negfib = list_negfib[:0:-1] + list_fib
+print(list_negfib)
